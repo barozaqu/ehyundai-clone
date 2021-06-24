f3323fb2b47999d63041a6e4e9a54caadfd2766d
barozaqu <barozaqu@gmail.com>
2021-06-24 14:21:04 +0900

    main banner : slick slider
    버튼 수정 필요

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+$(function(){
+
+    // basic slider
+    $('.main__banner').slick({
+        dots: true,
+        // 자동으로 돌아가게
+        autoplay: true,
+        autoplaySpeed: 2000
+    });
+});
